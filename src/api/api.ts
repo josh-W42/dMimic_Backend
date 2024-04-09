be95381fc5830915344bb873ac6f53c3e07f5d74
josh-W42 <wilson.jd022@gmail.com>
2024-04-09 12:33:23 -0700

    formatting

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import { mainRouter } from "../v1/routes";
-import { DB } from "../db/class";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { mainRouter } from '../v1/routes';
+import { DB } from '../db/class';
 
 export class DMimicService {
   private app: express.Express;
   private port: string | number = process.env.PORT || 8000;
-  private readonly BaseURL: string = process.env.BaseURL || "/api/";
-  private readonly APIversion: string = process.env.APIversion || "v1";
+  private readonly BaseURL: string = process.env.BaseURL || '/api/';
+  private readonly APIversion: string = process.env.APIversion || 'v1';
   private db: DB;
 
   constructor() {
@@ -17,6 +17,7 @@ export class DMimicService {
   }
 
   public Init() {
+    // Helps reduce fingerprinting.
     this.app.disable('x-powered-by');
 
     this.app.use(morgan('combined'));
@@ -27,7 +28,7 @@ export class DMimicService {
 
     this.app.use(`${this.BaseURL}${this.APIversion}`, mainRouter);
     this.app.listen(this.port, () => {
-      console.log("API Running on PORT: ", this.port);
+      console.log('API Running on PORT: ', this.port);
     });
   }
 
